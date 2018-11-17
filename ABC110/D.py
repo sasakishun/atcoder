0f0c2a50e3282346a9adcae0e4da93390fa1ea4c
sasakishun <Pakka-xeno@keio.jp>
2018-11-18 02:18:06 +0900

    add ABC110CD 113D

--- ABC110/D.py
+++ ABC110/D.py
@@ -1,2 +1,86 @@
-n = int(input())
-a = [int(i) for i in input().split()]
+import math
+
+
+def factorization(n):
+    R = int(n)
+    s = 0
+    L = []
+    div = 2
+    while s == 0:
+        for i in range(div, R + 1):
+            if n % i == 0:
+                n = n / i
+                div = i
+                if n == 1:
+                    s = 1
+                L.append(i)
+                break
+    return L
+
+
+def cmb(n, r):
+    if n - r < r:
+        r = n - r
+    if r == 0:
+        return 1
+    if r == 1:
+        return n;
+
+    numerator = [n - r + k + 1 for k in range(r)]
+    denominator = [k + 1 for k in range(r)]
+
+    for p in range(2, r + 1):
+        pivot = denominator[p - 1]
+        if pivot > 1:
+            offset = (n - r) % p;
+            for k in range(p - 1, r, p):
+                numerator[k - offset] /= pivot
+                denominator[k] /= pivot
+
+    result = 1
+    for k in range(r):
+        if numerator[k] > 1:
+            result *= int(numerator[k])
+
+    return result;
+
+
+"""
+def combinations_count(n, r):
+    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+
+
+def cmb(n, r, mod):
+    if r < 0 or r > n:
+        return 0
+    r = min(r, n - r)
+    return g1[n] * g2[r] * g2[n - r] % mod
+
+
+mod = 10 ** 9 + 7  # 出力の制限
+N = 10 ** 6
+g1 = [1, 1]  # 元テーブル
+g2 = [1, 1]  # 逆元テーブル
+inverse = [0, 1]  # 逆元テーブル計算用テーブル
+
+for i in range(2, N + 1):
+    g1.append((g1[-1] * i) % mod)
+    inverse.append((-inverse[mod % i] * (mod // i)) % mod)
+    g2.append((g2[-1] * inverse[-1]) % mod)
+
+"""
+n, m = [int(i) for i in input().split()]
+div = factorization(m)
+divList = []
+prevDiv = 0
+for i in range(len(div)):
+    if div[i] == prevDiv:
+        divList[-1] += 1
+    else:
+        divList.append(1)
+        prevDiv = div[i]
+count = 1
+for i in range(len(divList)):
+    count *= cmb(divList[i] + n - 1, divList[i])
+    # count *= combinations_count(divList[i] + n - 1, divList[i])
+print(count % (10 ** 9 + 7))
--- ABC113/D.py
+++ ABC113/D.py
@@ -1,8 +1,45 @@
 h, w, k = [int(i) for i in input().split()]
+h += 1
+if w == 1:
+    print(1)
+    exit()
+edge = [0 for i in range(w)]
+edge[0] = 1
+edge[1] = 2
+for i in range(2, w):
+    edge[i] = edge[i - 1] + edge[i - 2]
+# print("edge:{}".format(edge))
 
-dp = [[1 for j in range(w)] for i in range(h)]
-for i in range(1, h):
+dp = [[0 for j in range(w)] for i in range(h)]
+dp[0][0] = 1
+dp[0][1] = 0
+for i in range(h - 1):
     for j in range(w):
-        dp[i+1][j] = dp[i][j]*((w-3)**2)
-        dp[i+1][j-1] = dp[i][j]*2
-        dp[i+1][j+1] = dp[i][j]*2
+        add = 1
+        if 0 <= j - 1 < w:  # 小さい側
+            add *= edge[j - 1]
+            # print(add)
+        if 0 <= w - j - 2 < w:  # 大きい側
+            add *= edge[w - j - 2]
+            # print(add)
+        dp[i + 1][j] += dp[i][j] * add
+        # print("h:{} j:{} j:{}".format(i, j, add))
+        if j - 1 >= 0:
+            add = 1
+            if 0 <= j - 2 < w:
+                add *= edge[j - 2]
+            if 0 <= w - j - 2 < w:
+                add *= edge[w - j - 2]
+            dp[i + 1][j - 1] += dp[i][j] * add
+            # print("h:{} j:{} j-1:{}".format(i, j, add))
+        if j + 1 < w:
+            add = 1
+            if 0 <= j - 1 < w:
+                add *= edge[j - 1]
+            if 0 <= w - j - 3 < w:
+                add *= edge[w - j - 3]
+            dp[i + 1][j + 1] += dp[i][j] * add
+            # print("h:{} j:{} j+1:{}".format(i, j, add))
+        # for i in range(h):
+        # print(dp[i])
+print(dp[-1][k - 1] % 1000000007)

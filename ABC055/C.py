c7e61d6b9ca6034853bb5d82425f620818ed3f88
sasakishun <Pakka-xeno@keio.jp>
2018-11-24 19:24:39 +0900

    add ABC055-058C

--- ABC055/C.py
+++ ABC055/C.py
@@ -1,2 +1,16 @@
-n = int(input())
-a = [int(i) for i in input().split()]
+import math
+
+n, m = [int(i) for i in input().split()]
+# c:「m-2」とすれば,s:「n+1」と出来る
+count = 0
+# if int((m-2*n)/4) >= 0:
+if m - 2*n > 0:
+    count = max(count, min(n + int((m - 2 * n) / 4),
+                           int(m / 2 - (m - 2 * n) / 4)))
+    # print(count)
+    # if math.ceil((m-2*n)/4) >= 0:
+    count = max(count, min(n + math.ceil((m - 2 * n) / 4),
+                           int(m / 2) - math.ceil((m - 2 * n) / 4)))
+    # print(count)
+count = max(count, min(n, int(m / 2)))
+print(count)
--- ABC056/C.py
+++ ABC056/C.py
@@ -1,2 +1,9 @@
 n = int(input())
-a = [int(i) for i in input().split()]
+
+i = 0
+while True:
+    if n <= int((i*(i+1))/2):
+        print(i)
+        exit()
+    else:
+        i += 1

8a26c5b8857c57c370b495f25a159dba5e35dd0f
sasakishun <Pakka-xeno@keio.jp>
2019-01-16 01:01:12 +0900

    update ABC015D

--- ABC/ABC105/D.py
+++ ABC/ABC105/D.py
@@ -1,18 +1,35 @@
+import math
+def cmb(n, r):
+    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+
+def func(n, m, a):
+    # print("n:{} m:{} a:{}".format(n, m, a))
+    for i in range(n):
+        if i > 0:
+            a[i] += a[i-1]
+        a[i] %= m
+    a.sort()
+    a.append(-1)
+    # print(a)
+    count = 0
+    same = 1
+    for i in range(len(a)):
+        if a[i] == 0:
+            count += 1
+        if a[i] == a[i-1]:
+            same += 1
+        else:
+            if same >= 2:
+                count += cmb(same, 2)
+            same = 1
+    return count
+
 n, m = [int(i) for i in input().split()]
 a = [int(i) for i in input().split()]
-
-count = 0
-tmp_mod = 0
-under = 0
-for i in range(n):
-    tmp_mod += (i % m)
-    tmp_mod %= m
-
-    if tmp_mod % m == 0:
-        count += 1
-    elif tmp_mod > m:
-        tmp_mod -= a[under] % m
-        tmp_mod %= m
-        under += 1
-    print("{}-{} mod:{}".format(under, i, tmp_mod))
-print(count)
+print(func(n, m, a))
+"""
+print(func(3, 2, [4, 1, 5]))
+print(func(13, 17, [29, 7, 5, 7, 9, 51, 7, 13, 8, 55, 42, 9, 81]))
+print(func(10, 400000000, [1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000]))
+"""
+# print("func({}, {}, {})".format(n, m, a))

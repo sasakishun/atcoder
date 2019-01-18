5204fc2b2b90c735ed67a8e2728db6f1416b1f96
sasakishun <Pakka-xeno@keio.jp>
2019-01-18 23:59:47 +0900

    update ABC088, 089 and 097

--- ABC/ABC089/D-Practical_Skill_Test.py
+++ ABC/ABC089/D-Practical_Skill_Test.py
@@ -1,35 +1,36 @@
+import math
+def func(h, w, d, a, query):
+    imos = [0 for _ in range(h*w)]
+    pos = [[0, 0] for _ in range(h*w)]
+    for i in range(h):
+        for j in range(w):
+            pos[a[i][j]] = [i, j]
+    for i in range(d):
+        for j in range(math.ceil((h*w-i)/d)):
+            if j == 0:
+                continue
+            else:
+                imos[i + d * j] = imos[i + d * (j-1)]\
+                                  + abs(pos[i + d * j][0] - pos[i + d * (j-1)][0])\
+                                  + abs(pos[i + d * j][1] - pos[i + d * (j-1)][1])
+    for i in range(len(query)):
+        print(imos[query[i][1]] - imos[query[i][0]])
+    return
 h, w, d = [int(i) for i in input().split()]
-a = [[int(j)-1 for j in input().split()] for i in range(h)]
-
-q = int(input())#moving num
-l = [0 for i in range(q)]#start number
-r = [0 for i in range(q)]#finish number
-
-for i in range(q):
-    tmp = input().split()
-    l[i]=int(tmp[0])-1
-    r[i]=int(tmp[1])-1
-loc = [[0, 0] for i in range(h*w)]
+a = [[0 for _ in range(w)] for _ in range(h)]
 for i in range(h):
-    for j in range(w):
-        loc[a[i][j]][0] = i
-        loc[a[i][j]][1] = j
-#print(loc)
-cost = [0 for i in range(int((h*w)/d))]
-#print(cost)
-for i in range(1, len(cost)):
-    cost[i] += abs(loc[(i+1)*d-1][0]-loc[i*d-1][0])
-    cost[i] += abs(loc[(i+1)*d-1][1]-loc[i*d-1][1])
-#print(cost)
-for i in range(1, len(cost)):
-    cost[i] += cost[i-1]
-#print(cost)
-
+    a[i] = [int(i) - 1 for i in input().split()]
+q = int(input())
+query = [[0, 0] for _ in range(q)]
 for i in range(q):
-    if (l[i]+1)%d==0:
-        print(cost[int(r[i]/d)]-cost[int(l[i]/d)])
-    else:#recite
-        print(cost[int(r[i]/d)]-cost[int(l[i]/d)+1]
-              +abs(loc[int(l[i]/d)][0]-loc[l[i]][0])
-              +abs(loc[int(l[i]/d)][1]-loc[l[i]][1])
-        )
+    query[i] = [int(i) - 1 for i in input().split()]
+func(h, w, d, a, query)
+"""
+func(3, 3, 2, [[0, 3, 2], [1, 4, 6], [7, 8, 5]], [[3, 7]])
+print("ans: 5\n")
+func(4, 2, 3, [[2, 6], [0, 3], [4, 1], [5, 7]], [[1, 1], [1, 1]])
+print("ans: 0, 0\n")
+func(5, 5, 4, [[12, 24, 6, 14, 16], [15, 21, 19, 1, 8], [13, 10, 11, 0, 18], [9, 5, 22, 7, 17], [2, 20, 4, 23, 3]], [[12, 12], [1, 9], [12, 12]])
+print("ans: 0, 5, 0")
+"""
+# print("func({}, {}, {}, {}, {})".format(h, w, d, a, query))

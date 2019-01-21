073d921422fc41cd140e1af0ca9229a56a99520e
sasakishun <Pakka-xeno@keio.jp>
2019-01-21 22:45:56 +0900

    add ABC073D

--- ABC/ABC073/D.py
+++ ABC/ABC073/D.py
@@ -1,2 +1,27 @@
-n = int(input())
-a = [int(i) for i in input().split()]
+import itertools
+n, m, R = map(int, input().split())
+r = list(map(int, input().split()))
+dis = [[float("inf") for _ in range(n)] for _ in range(n)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    dis[a - 1][b - 1] = c
+    dis[b - 1][a - 1] = c
+
+def war(dis):
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dis[i][j] > dis[i][k] + dis[k][j]:
+                    dis[i][j] = dis[i][k] + dis[k][j]
+    return dis
+dis = war(dis)
+_min = float("inf")
+it = itertools.permutations(r)
+for iterR in it:
+    _dis = 0
+    start = iterR[0] - 1
+    for end in iterR[1:]:
+        _dis += dis[start][end - 1]
+        start = end - 1
+    _min = min(_min, _dis)
+print(_min)
--- ABC/ABC075/D.py
+++ ABC/ABC075/D.py
@@ -1,5 +1,51 @@
-inf = 10**20
-def func(n, k, pos):
+inf = 10 ** 20
+
+
+def func(n, _k, pos):
+    x = [pos[i][0] for i in range(len(pos))]
+    y = [pos[i][1] for i in range(len(pos))]
+    x.sort()
+    y.sort()
+    imos = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if pos[i][0] == x[j] and pos[i][1] == y[k]:
+                    imos[j][k] += 1
+    for i in range(n):
+        for j in range(1, n):
+            imos[i][j] += imos[i][j - 1]
+    for i in range(1, n):
+        for j in range(n):
+            imos[i][j] += imos[i - 1][j]
+    # for i in reversed(range(len(imos))):
+        # print(imos[i])
+
+    def getImos(_right, _left, _upper, _bottom):
+        _sum = imos[_right][_upper]
+        if _bottom > 0:
+            _sum -= imos[_right][_bottom - 1]
+        if _left > 0:
+            _sum -= imos[_left - 1][_upper]
+        if _bottom > 0 and _left > 0:
+            _sum += imos[_left - 1][_bottom - 1]
+        return _sum
+
+    _min = inf
+    for left in range(len(x) - 1):
+        for right in range(left + 1, len(x)):
+            for bottom in range(len(y) - 1):
+                for upper in range(bottom + 1, len(y)):
+                    """
+                    print("{} {} {} {} :{}poi s:{}".format(right, left, bottom, upper,
+                                                        getImos(right, left, upper, bottom),
+                                                        (x[right] - x[left]) * (y[upper] - y[bottom])))
+                    """
+                    if getImos(right, left, upper, bottom) >= _k:
+                        _min = min(_min, (x[right] - x[left]) * (y[upper] - y[bottom]))
+                        # print("s:{}".format((x[right] - x[left]) * (y[upper] - y[bottom])))
+    return _min
+    """
     out = inf
     for i in range(n):
         # pos[i]を含む長方形を考える
@@ -28,6 +74,9 @@ def func(n, k, pos):
             # print(minS)
         out = min(out, minS)
     return out
+    """
+
+
 n, k = [int(i) for i in input().split()]
 pos = [[0, 0] for _ in range(n)]
 for i in range(n):
@@ -36,6 +85,7 @@ print(func(n, k, pos))
 """
 print("21 vs " + str(func(4, 4, [[1, 4], [3, 3], [6, 2], [8, 1]])))
 print("1 vs " + str(func(4, 2, [[0, 0], [1, 1], [2, 2], [3, 3]])))
-print("3999999996000000001 vs " + str(func(4, 3, [[-1000000000, -1000000000], [1000000000, 1000000000], [-999999999, 999999999], [999999999, -999999999]])))
-# print("print(\"\"+str(func({}, {}, {})))".format(n, k, pos))
+print("3999999996000000001 vs " + str(func(4, 3, [[-1000000000, -1000000000], [1000000000, 1000000000],
+                                                  [-999999999, 999999999], [999999999, -999999999]])))
 """
+# print("print(\"\"+str(func({}, {}, {})))".format(n, k, pos))

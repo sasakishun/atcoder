c0b1d26f5f7c4ffc862cb3f0067a33d6d5db4555
sasakishun <Pakka-xeno@keio.jp>
2019-01-25 22:17:51 +0900

    add ABC061D ABC063D ABC064D

--- ABC/ABC061/D.py
+++ ABC/ABC061/D.py
@@ -1,2 +1,42 @@
-n = int(input())
-a = [int(i) for i in input().split()]
+import heapq
+
+
+class dijkstra:
+    def __init__(self, adj):
+        self.adj = adj
+        self.visited = [-1 for _ in range(len(adj))]
+        self.heap = []
+
+    def allSearch(self, _start):
+        heapq.heappush(self.heap, [0, _start])
+        while 1:
+            if len(self.heap) == 0:
+                break
+            start = heapq.heappop(self.heap)
+            # 同じノードへ複数候補がある場合がある、
+            # 一度訪れたノードへの候補は削除する
+            if self.visited[start[1]] != -1:
+                continue
+            # 確定した距離をvisitテーブルに保持
+            self.visited[start[1]] = start[0]
+            for i in range(len(self.adj)):
+                if -self.adj[start[1]][i] - self.visited[start[1]] < -self.visited[i]:
+                    heapq.heappush(self.heap, [-(self.adj[start[1]][i] + self.visited[start[1]]), i])
+                if self.visited[i] == -1 and self.adj[start[1]][i] != 0:
+                    heapq.heappush(self.heap,
+                                   [-(self.adj[start[1]][i] + self.visited[start[1]]),
+                                    i,
+                                    self.adj[start[1]][i] + self.visited[start[1]]])
+        # print("start:{} score:{}".format(_start, max(self.visited)))
+        return self.visited
+
+
+_min = -10 ** 12
+n, m = [int(i) for i in input().split()]
+adj = [[_min for _ in range(n)] for _ in range(n)]
+for _ in range(m):
+    a, b, c = [int(i) for i in input().split()]
+    adj[a - 1][b - 1] = c
+dij = dijkstra(adj)
+dij.allSearch(0)
+print(dij.visited[0][-1])
--- ABC/ABC064/D.py
+++ ABC/ABC064/D.py
@@ -1,2 +1,24 @@
 n = int(input())
-a = [int(i) for i in input().split()]
+s = input()
+
+left = 0
+right = 0
+out = ""
+for i in range(len(s)):
+    if s[i] == "(":
+        left += 1
+        out += "("
+    else:
+        right += 1
+        out += ")"
+        if right > left:
+            out = "(" + out
+            left += 1
+while left != right:
+    if right > left:
+        left += 1
+        out = "(" + out
+    else:
+        right += 1
+        out += ")"
+print(out)
